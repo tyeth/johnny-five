c6f444c4a507839b0204ac3867ed4c4948a2a15f
tyeth <tyeth@lelan.co.uk>
2016-06-09 09:45:37 +0100

    Adapted for raspberry pi and lastTimestamp

--- eg/motion.js
+++ eg/motion.js
@@ -1,11 +1,14 @@
 var five = require("../lib/johnny-five.js");
-var board = new five.Board();
+var raspi = require("raspi-io");
+var board = new five.Board({io: new raspi()});
+var pir = [];
+
 
 board.on("ready", function() {
 
   // Create a new `motion` hardware instance.
-  var motion = new five.Motion(7);
-
+  var motion = new five.Motion('P1-23');
+  pir[0] = new {motionSensor: motion, data: {}, lastValueTimestamp: -1, lastQuietTimestamp:-1, value: false,  }; //activity: (function(){return value;})()
   // "calibrated" occurs once, at the beginning of a session,
   motion.on("calibrated", function() {
     console.log("calibrated");
@@ -15,12 +18,16 @@ board.on("ready", function() {
   // proximal area is disrupted, generally by some form of movement
   motion.on("motionstart", function() {
     console.log("motionstart");
+    pir[0].lastValueTimestamp = Date().now;
+    value = true;
   });
 
   // "motionend" events are fired following a "motionstart" event
   // when no movement has occurred in X ms
   motion.on("motionend", function() {
     console.log("motionend");
+    pir[0].lastQuietTimestamp = Date().now;
+    value = false;
   });
 
   // "data" events are fired at the interval set in opts.freq
